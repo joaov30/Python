ec1c512c3c2ca2fa14ba2224242119ea482fbaa4
João Victor C Baptista <76636099+joaov30@users.noreply.github.com>
2021-05-30 16:34:09 -0300

    Update adivinhacao.py

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,1 +1,17 @@
+print("*********************************")
 print("Bem vindo no jogo de adivinhação!")
+print("*********************************")
+
+numero_secreto = 50
+
+chute_str = input("Digite o seu numero: ")
+
+print("Você digitou ", chute_str)
+
+chute = int(chute_str)
+
+if(numero_secreto == chute):
+    print("Você acertou!")
+else:
+    print("Você errou!")
+
