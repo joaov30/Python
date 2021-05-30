f79d58fbece04142650e17a0d400c0ae17203c4c
João Victor C Baptista <76636099+joaov30@users.noreply.github.com>
2021-05-30 19:08:17 -0300

    Update adivinhacao.py

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,27 +1,21 @@
-print("*********************************")
-print("Bem vindo no jogo de adivinhação!")
-print("*********************************")
+print("Jogo da advinhação")
+print("******************")
 
-numero_secreto = 50
+numero_secreto = 30
 
-chute_str = input("Digite o seu numero: ")
+palpite_str = input("Digite o seu palpite: ")
 
-print("Você digitou ", chute_str)
+palpite = int(palpite_str)
 
-chute   = int(chute_str)
+maior = palpite > numero_secreto
 
-acertou = chute == numero_secreto
+menor = palpite < numero_secreto
 
-maior   = chute > numero_secreto
-
-menor   = chute < numero_secreto
-
-if acertou:
+if palpite == numero_secreto:
     print("Você acertou!")
-else:
-    if maior:
-        print("Você errou! O seu chute foi maior do que o número secreto.")
-    elif menor:
-        print("Você errou! O seu chute foi menor do que o número secreto.")
+elif maior:
+    print("Seu palpite ultrapassou o numero secreto")
+elif menor:
+    print("Seu palpite foi menor do que o numero secreto")
 
-print("Game over")
+print("Game Over")
