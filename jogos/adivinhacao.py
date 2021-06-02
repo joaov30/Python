4700c5aa401ca27e082844eefab43a24499c27c8
João Victor C Baptista <76636099+joaov30@users.noreply.github.com>
2021-06-02 20:13:05 -0300

    Update adivinhacao.py

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -5,13 +5,25 @@ print("Bem vindo ao Jogo da advinhação")
 print("******************************")
 
 numero_secreto = random.randrange(1, 101)
-contador = 3
+contador = 0
+pontos = 100
 
-print(numero_secreto)
+print("Qual o nível de dificuldade", numero_secreto)
+print("(1) Facil, (2) Médio, (3) Dificil")
+
+nivel = int(input("Defina o nível de dificuldade: "))
+
+if (nivel == 1):
+    contador = 20
+elif(nivel == 2):
+    contador = 10
+else:
+    contador = 5
 
 
 for rodada in range(1, contador + 1):
     print("Tentativa {} de {}".format( rodada, contador))
+
     palpite_str = input("Digite o seu palpite entre 1 e 100: ")
     print("Você digitou ", palpite_str)
     palpite = int(palpite_str)
@@ -26,7 +38,7 @@ for rodada in range(1, contador + 1):
 
     if (acertou):
         print("*************")
-        print("Você acertou!")
+        print("Você acertou e fez {} pontos!".format(pontos))
         print("*************")
         break
     else:
@@ -34,6 +46,8 @@ for rodada in range(1, contador + 1):
             print("Seu palpite ultrapassou o numero secreto")
         elif (menor):
             print("Seu palpite foi menor do que o numero secreto")
+        pontos_perdidos = abs(numero_secreto - palpite)
+        pontos = pontos - pontos_perdidos
 
 
 
