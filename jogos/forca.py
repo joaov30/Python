f95dc4ad0574f0581f86243e265e3eca42f94020
João Victor C Baptista <76636099+joaov30@users.noreply.github.com>
2021-06-04 23:50:12 -0300

    Update forca.py

--- jogos/forca.py
+++ jogos/forca.py
@@ -4,6 +4,13 @@ def jogar():
     print("Bem vindo ao jogo de Forca!")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        print("jogando...")
+
     print("Fim do jogo")
 
 if(__name__== "__main__"):
